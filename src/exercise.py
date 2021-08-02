314e81bb12119e516a5683020a4091eaf16e78ac
Justin Turner <jmturner638@gmail.com>
2021-08-02 11:15:11 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,23 @@
 def main():
     #write your code below this line
+    oldest_age = 0
+    oldest_name = ''
+
+    while True:
+        line = input()
+
+        if line == '':
+            break
+
+        data = line.split(',')
+        name = data[0]
+        age = int(data[1])
+
+        if age > oldest_age:
+            oldest_age = age
+            oldest_name = name
+
+    print("Name of the oldest: " + oldest_name)
 
 if __name__ == '__main__':
     main()
